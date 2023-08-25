d2bcf70a3842dde7c1b50a273a0d1f5b628a20fb
Thijs Putman <thijs@sgraastra.net>
2023-08-25 13:41:38 +0200

    Apply shfmt formatting suggestions
    Mostly minor and sensible (mainly spacing around ";" and "{", and some
    incorrect indents). Some of the more complicated heredoc/subshell
    combinations tripped things up slightly – those have been reworked
    (and in general now have improved clarity).

--- extras/sysmon-mqtt/sysmon.sh
+++ extras/sysmon-mqtt/sysmon.sh
@@ -22,11 +22,11 @@ fi
 
 # Compute number of ticks per hour; additionally, forces $SYSMON_INTERVAL to
 # base10 — exits in case of an invalid value for the interval
-hourly_ticks=$((3600/10#$SYSMON_INTERVAL))
+hourly_ticks=$((3600 / 10#$SYSMON_INTERVAL))
 
 # APT-related metrics make no sense when running inside a Docker-container or
 # when APT is not present on the system
-if [ "$SYSMON_IN_DOCKER" = true ] || ! command -v apt &> /dev/null ; then
+if [ "$SYSMON_IN_DOCKER" = true ] || ! command -v apt &> /dev/null; then
   SYSMON_APT=false
 fi
 
@@ -65,7 +65,7 @@ goodbye() {
 # Clean parameters to be used in MQTT-topics and JSON-keys — reduce them to
 # lowercase alphanumeric and underscores; exit if nothing remains
 
-mqtt_json_clean(){
+mqtt_json_clean() {
 
   param="${1:?"Missing parameter to clean!"}"
 
@@ -76,17 +76,18 @@ mqtt_json_clean(){
   # agreeable output (assuming anything which remotely resembles an IP-address
   # is actually one).
 
-  if [[ "$param" =~ ^[0-9.]+$ ]] ; then
+  if [[ "$param" =~ ^[0-9.]+$ ]]; then
     param="IP $param"
-  elif [[ "$param" =~ ^[0-9]+$ ]] ; then
+  elif [[ "$param" =~ ^[0-9]+$ ]]; then
     param="N $param"
   fi
 
   param=$(echo "${param//[^A-Za-z0-9_ -.]/}" |
     tr -s ' -.' _ | tr '[:upper:]' '[:lower:]')
 
-  if [ -z "$param" ] ; then
-    echo "Invalid parameter '$1' supplied!" ; exit 1
+  if [ -z "$param" ]; then
+    echo "Invalid parameter '$1' supplied!"
+    exit 1
   fi
 
   echo "$param"
@@ -105,7 +106,7 @@ mosquitto_pub -r -q 1 -h "$mqtt_host" \
 
 # Construct Home Assistant discovery-payload
 
-ha_discover(){
+ha_discover() {
 
   local name=${1}
   local attribute=${2}
@@ -117,16 +118,18 @@ ha_discover(){
 
   # Attempt to retrieve existing UUID; otherwise generate a new one
 
-  if config=$(mosquitto_sub -h "$mqtt_host" -C 1 -W 3 \
-    -t "${ha_topic}/$entity/sysmon/${device}_${attribute//\//_}/config" \
-    2> /dev/null) ; then
-    unique_id=$(jq -r -c '.unique_id' <<< "$config" 2> /dev/null) \
-      || true # This ensures invalid JSON-payloads are ignored
+  if config=$(
+    mosquitto_sub -h "$mqtt_host" -C 1 -W 3 \
+      -t "${ha_topic}/$entity/sysmon/${device}_${attribute//\//_}/config" \
+      2> /dev/null
+  ); then
+    unique_id=$(jq -r -c '.unique_id' <<< "$config" 2> /dev/null) ||
+      true # This ensures invalid JSON-payloads are ignored
   else
     unique_id=""
   fi
 
-  if ! [[ "$unique_id" =~ ^[0-9a-z-]{36}$ ]] ; then
+  if ! [[ "$unique_id" =~ ^[0-9a-z-]{36}$ ]]; then
     unique_id=$(< /proc/sys/kernel/random/uuid)
   fi
 
@@ -135,11 +138,11 @@ ha_discover(){
   local device_class=""
   local icon=""
 
-  if [ -n "${class_icon[0]}" ] ; then
-    if [ "${#class_icon[@]}" -gt 1 ] ; then
-        device_class="${class_icon[0]}"
-        icon="${class_icon[1]}"
-    elif [[ "${class_icon[0]}" =~ ^mdi: ]] ; then
+  if [ -n "${class_icon[0]}" ]; then
+    if [ "${#class_icon[@]}" -gt 1 ]; then
+      device_class="${class_icon[0]}"
+      icon="${class_icon[1]}"
+    elif [[ "${class_icon[0]}" =~ ^mdi: ]]; then
       icon="${class_icon[0]}"
     else
       device_class="${class_icon[0]}"
@@ -152,16 +155,18 @@ ha_discover(){
   # handles edge cases (MQTT component/HA reload) more gracefully though...
 
   local value_template
-  value_template=$(tr -d '\n' <<- EOF
+  value_template=$(
+    tr -d '\n' <<- EOF
     value_json.${attribute//\//.} | float(0) | round($((10#$precision)))
 		EOF
   ) # N.B., EOF-line should be indented with tabs!
   local state_topic="sysmon/$device/state"
-  local expire_after=$((10#$SYSMON_INTERVAL*3))
+  local expire_after=$((10#$SYSMON_INTERVAL * 3))
   local entity_picture=""
   local state_class=""
   local availability_template
-  availability_template=$(tr -d '\n' <<- EOF
+  availability_template=$(
+    tr -d '\n' <<- EOF
     'online' if (value | int(0) | as_datetime) + timedelta(
       seconds = ${expire_after}) >= now() else 'offline'
 		EOF
@@ -169,20 +174,21 @@ ha_discover(){
 
   # Heartbeat and APT have somewhat different setups
 
-  if [ "$attribute" = "heartbeat" ] ; then
+  if [ "$attribute" = "heartbeat" ]; then
     expire_after=0
     state_topic="sysmon/$device/connected"
     value_template="(value | int(0) | as_datetime)"
-  elif [ "$attribute" = "apt" ] ; then
+  elif [ "$attribute" = "apt" ]; then
     expire_after=0
-    if command -v lsb_release &> /dev/null ; then
-      entity_picture="/local/sysmon-mqtt/$(lsb_release -ds | cut -d ' ' -f1 | \
+    if command -v lsb_release &> /dev/null; then
+      entity_picture="/local/sysmon-mqtt/$(lsb_release -ds | cut -d ' ' -f1 |
         tr '[:upper:]' '[:lower:]').png"
     fi
     value_template="value_json.${attribute//\//.} | to_json"
-  elif [ "$attribute" = "reboot_required" ] ; then
+  elif [ "$attribute" = "reboot_required" ]; then
     expire_after=0
-    value_template=$(tr -d '\n' <<- EOF
+    value_template=$(
+      tr -d '\n' <<- EOF
       'ON' if (value_json.${attribute//\//.} | int(0)) == 1 else 'OFF'
 			EOF
     ) # N.B., EOF-line should be indented with tabs!
@@ -190,51 +196,55 @@ ha_discover(){
 
   # Set state_class to "measurement" if unit_of_measurement is defined; in case
   # of the Uptime-sensor set it to "total_increasing".
-  if [ -n "$unit" ] ; then
+  if [ -n "$unit" ]; then
     state_class="measurement"
-    if [ "$attribute" == uptime ] ; then
+    if [ "$attribute" == uptime ]; then
       state_class="total_increasing"
     fi
   fi
 
   # For expiry of 0, the "simple" behaviour is actually what we want...
-  if [ "$expire_after" -eq 0 ] ; then
+  if [ "$expire_after" -eq 0 ]; then
     availability_template="'online' if value | int(0) > 0 else 'offline'"
   fi
 
-  local payload
-  # For "model", the use of cat is intentional (it redirects "not found"-errors
-  # to /dev/null). Furthermore, no model is reported in (non-privileged) Docker-
+  local payload_name payload_model
+  payload_name=$(
+    {
+      [ "$((10#$SYSMON_HA_VERSION))" -lt 202308 ] &&
+        printf "%s " "$device_name"
+      printf "%s" "$name"
+    } | jq -R -s '.'
+  )
+  # The use of cat is intentional here (it redirects "not found"-errors to
+  # /dev/null). Furthermore, no model is reported in (non-privileged) Docker-
   # containers while <https://github.com/moby/moby/issues/43419> remains open...
   # shellcheck disable=SC2002
-  payload=$(tr -s ' ' <<- EOF
+  payload_model=$(
+    cat /sys/firmware/devicetree/base/model 2> /dev/null | tr -d '\0' || true
+  )
+
+  local payload
+  payload=$(
+    tr -s ' ' <<- EOF
     {
-      "name": $(
-        {
-          [ "$((10#$SYSMON_HA_VERSION))" -lt 202308 ] && \
-            printf "%s " "$device_name"
-          printf "%s" "$name" ;
-        } | jq -R -s '.'
-      ),
+      "name": $payload_name,
       "object_id": "${device}_${attribute//\//_}",
       "unique_id": "$unique_id",
       "device": {
           "identifiers": "sysmon_${device}",
           "name": $(echo -n "$device_name" | jq -R -s '.'),
           "manufacturer": "sysmon-mqtt",
-          "model": "$(cat /sys/firmware/devicetree/base/model \
-            2> /dev/null | tr -d '\0' || true)",
+          "model": "$payload_model",
           "sw_version": "$(uname -smr)"
       },
       $([ -n "$device_class" ] && echo "\"device_class\": \"$device_class\",")
       $([ -n "$icon" ] && echo "\"icon\": \"$icon\",")
-      $([ -n "$entity_picture" ] && \
-        echo "\"entity_picture\": \"$entity_picture\",")
+      $([ -n "$entity_picture" ] && echo "\"entity_picture\": \"$entity_picture\",")
       "state_topic": "$state_topic",
       $([ -n "$unit" ] && echo "\"unit_of_measurement\": \"$unit\",")
       $([ -n "$state_class" ] && echo "\"state_class\": \"$state_class\",")
-      $([ -n "$value_template" ] && \
-        echo "\"value_template\": \"{{ $value_template }}\",")
+      $([ -n "$value_template" ] && echo "\"value_template\": \"{{ $value_template }}\",")
       "expire_after": "$expire_after",
       "availability": {
         "topic": "sysmon/$device/connected",
@@ -251,7 +261,7 @@ ha_discover(){
     -m "$payload" || true
 }
 
-if [ "$SYSMON_HA_DISCOVER" = true ] ; then
+if [ "$SYSMON_HA_DISCOVER" = true ]; then
 
   ha_discover Heartbeat heartbeat 'timestamp mdi:heart-pulse'
   ha_discover Uptime uptime 'duration mdi:timer-outline' s
@@ -259,7 +269,7 @@ if [ "$SYSMON_HA_DISCOVER" = true ] ; then
   ha_discover 'CPU load' cpu_load mdi:chip %
   ha_discover 'Memory usage' mem_used mdi:memory %
 
-  for eth_adapter in "${eth_adapters[@]}" ; do
+  for eth_adapter in "${eth_adapters[@]}"; do
 
     ha_discover "Bandwidth in (${eth_adapter})" "bandwidth/${eth_adapter}/rx" \
       'data_rate mdi:download-network' kbit/s
@@ -268,14 +278,14 @@ if [ "$SYSMON_HA_DISCOVER" = true ] ; then
 
   done
 
-  for rtt_host in "${rtt_hosts[@]}" ; do
+  for rtt_host in "${rtt_hosts[@]}"; do
 
     ha_discover "Round-trip time (${rtt_host})" \
       "rtt/$(mqtt_json_clean "$rtt_host")" mdi:server-network ms '' 3
 
   done
 
-  if [ "$SYSMON_APT" = true ] ; then
+  if [ "$SYSMON_APT" = true ]; then
     ha_discover 'APT upgrades' apt mdi:package-up '' update
     ha_discover 'Reboot required' reboot_required mdi:restart '' \
       binary_sensor
@@ -301,14 +311,15 @@ _readfd() {
 }
 
 cpu_cores=$(nproc --all)
-rx_prev=() ; tx_prev=()
+rx_prev=()
+tx_prev=()
 first_loop=true
 hourly=true
 ticks=0
 
 # APT-check output file (defaults to temporary file)
-if [ "$SYSMON_APT" = true ] ; then
-  if [ -n "$SYSMON_APT_CHECK" ] ; then
+if [ "$SYSMON_APT" = true ]; then
+  if [ -n "$SYSMON_APT_CHECK" ]; then
     touch "$SYSMON_APT_CHECK" && apt_check="$SYSMON_APT_CHECK"
   else
     apt_check=$(mktemp -t sysmon.apt-check.XXXXXXXX)
@@ -326,12 +337,12 @@ fi
 payload_rtt=""
 
 # ZFS ARC — minimum size
-if [ -f /proc/spl/kstat/zfs/arcstats ] ; then
+if [ -f /proc/spl/kstat/zfs/arcstats ]; then
   zfs_arc_min=$(awk '/^c_min/ {printf "%.0f", $3/1024 }' < \
     /proc/spl/kstat/zfs/arcstats)
 fi
 
-while true ; do
+while true; do
 
   # Uptime
   uptime=$(cut -d ' ' -f1 < /proc/uptime)
@@ -341,7 +352,7 @@ while true ; do
     /sys/class/thermal/thermal_zone0/temp)
 
   # Load (1-minute load / # of cores)
-  cpu_load=$(uptime | \
+  cpu_load=$(uptime |
     awk "match(\$0, /load average: ([0-9\.]*),/, \
       result){printf \"%3.2f\", result[1]*100/$cpu_cores}")
 
@@ -354,11 +365,11 @@ while true ; do
   # kernel in Linux. Approach taken from btop: If current ARC size is greater
   # than its minimum size (lower than which it'll never go), assume the surplus
   # to be available memory.
-  if [ -v zfs_arc_min ] && [ -n "$zfs_arc_min" ] ; then
+  if [ -v zfs_arc_min ] && [ -n "$zfs_arc_min" ]; then
     zfs_arc_size=$(awk '/^size/ {printf "%.0f", $3/1024}' < \
       /proc/spl/kstat/zfs/arcstats)
-    if [ "$zfs_arc_size" -gt "$zfs_arc_min" ] ; then
-      mem_avail=$((mem_avail+zfs_arc_size-zfs_arc_min))
+    if [ "$zfs_arc_size" -gt "$zfs_arc_min" ]; then
+      mem_avail=$((mem_avail + zfs_arc_size - zfs_arc_min))
     fi
   fi
 
@@ -369,7 +380,7 @@ while true ; do
 
   payload_bw=()
 
-  for i in "${!eth_adapters[@]}" ; do
+  for i in "${!eth_adapters[@]}"; do
 
     eth_adapter="${eth_adapters[i]}"
 
@@ -379,18 +390,22 @@ while true ; do
     tx=$(< "/sys/class/net/${eth_adapter%%/*}/statistics/tx_bytes")
 
     # Only run when "prev" is initialised
-    if [ "${#rx_prev[@]}" -eq "${#eth_adapters[@]}" ] ; then
+    if [ "${#rx_prev[@]}" -eq "${#eth_adapters[@]}" ]; then
 
-      payload_bw+=("$(tr -s ' ' <<- EOF
+      payload_rx=$(
+        awk '{printf "%3.2f", ($1-$2)/$3*8/1000}' \
+          <<< "$rx ${rx_prev[i]} $((10#$SYSMON_INTERVAL))"
+      )
+      payload_tx=$(
+        awk '{printf "%3.2f", ($1-$2)/$3*8/1000}' \
+          <<< "$tx ${tx_prev[i]} $((10#$SYSMON_INTERVAL))"
+      )
+
+      payload_bw+=("$(
+        tr -s ' ' <<- EOF
         "$eth_adapter": {
-          "rx": "$(
-            awk '{printf "%3.2f", ($1-$2)/$3*8/1000}' \
-              <<< "$rx ${rx_prev[i]} $((10#$SYSMON_INTERVAL))"
-          )",
-          "tx": "$(
-            awk '{printf "%3.2f", ($1-$2)/$3*8/1000}' \
-              <<< "$tx ${tx_prev[i]} $((10#$SYSMON_INTERVAL))"
-          )"
+          "rx": "$payload_rx",
+          "tx": "$payload_tx"
         }
 				EOF
       )") # N.B., EOF-line should be indented with tabs!
@@ -446,11 +461,12 @@ while true ; do
   payload_apt=()
   reboot_required=0
 
-  if [ -v apt_check ] ; then
+  if [ -v apt_check ]; then
 
-    if [ -s "$apt_check" ] ; then
+    if [ -s "$apt_check" ]; then
 
-      payload_apt+=("$(tr -s ' ' <<- EOF
+      payload_apt+=("$(
+        tr -s ' ' <<- EOF
         "title": "APT",
         "installed_version": "0",
         "latest_version": "$(head -n 1 "$apt_check")",
@@ -462,7 +478,7 @@ while true ; do
 
     # Run apt-check and its processing once per hour
 
-    if [ "$hourly" = true ] ; then
+    if [ "$hourly" = true ]; then
 
       : > "$apt_check"
 
@@ -476,13 +492,13 @@ while true ; do
 
         apt_upgrades=$(tail -n 1 <<< "$apt_simulate" | awk \
           'match($0, /([0-9]+) upgraded,/, result){printf "%d", result[1]}')
-        if [ -z "$apt_upgrades" ] ; then
+        if [ -z "$apt_upgrades" ]; then
           apt_upgrades=0
           apt_summary="\"No packages can be upgraded.\""
         else
           apt_summary=$(head -n -1 <<< "$apt_simulate" | tr -d -s '\n' ' ')
           apt_summary=$(printf '%s\n\n%s' \
-            "The following packages can be upgraded:" "${apt_summary:1:255}" | \
+            "The following packages can be upgraded:" "${apt_summary:1:255}" |
             jq -R -s '.')
         fi
 
@@ -493,7 +509,7 @@ while true ; do
 
     # Reboot-required
 
-    if [ -f /var/run/reboot-required ] ; then
+    if [ -f /var/run/reboot-required ]; then
       reboot_required=1
     fi
 
@@ -501,7 +517,8 @@ while true ; do
 
   # Construct payload
 
-  payload=$(tr -s ' ' <<- EOF
+  payload=$(
+    tr -s ' ' <<- EOF
     {
       "uptime": "$uptime",
       "cpu_temp": "$cpu_temp",
@@ -529,19 +546,22 @@ while true ; do
   # those while gathering the first set of metrics) are not trapped and will
   # leave the connected-state as "-1".
 
-  if [ "$first_loop" = false ] ; then
+  if [ "$first_loop" = false ]; then
 
     mosquitto_pub -r -q 1 -h "$mqtt_host" \
       -t "sysmon/$device/connected" -m "$(date +%s)" || true
 
-  else trap goodbye INT HUP TERM EXIT ; fi
+  else trap goodbye INT HUP TERM EXIT; fi
 
   first_loop=false
 
   # Track ticks and hourly-trigger
-  ticks=$((ticks+1))
+  ticks=$((ticks + 1))
   hourly=false
-  if [ "$ticks" -gt "$hourly_ticks" ] ; then hourly=true ; ticks=0 ; fi
+  if [ "$ticks" -gt "$hourly_ticks" ]; then
+    hourly=true
+    ticks=0
+  fi
 
   sleep "$((10#$SYSMON_INTERVAL))s" &
   wait $!
