ef7d7b4d1202b793b984a81cf9994358a8da82e4
Thijs Putman <thijs@sgraastra.net>
2023-08-26 20:42:11 +0200

    🙏 Swap over "Lint codebase"-workflow to pre-commit
    As as tradition, I have no clue if this is actually going to work
    properly, but we'll see soon enough...

---
+++ .github/scripts/setup-pre-commit.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pip3 install pre-commit
+pip3 install yamllint
+
+# hadolint
+
+if [ ! -x "${HOME}/.local/bin/hadolint" ]; then
+
+  hadolint_base=https://github.com/hadolint/hadolint/releases/download
+  wget -nv -O "${HOME}/.local/bin/hadolint" \
+    "${hadolint_base}/${HADOLINT_VERSION}/hadolint-Linux-x86_64"
+  chmod +x "${HOME}/.local/bin/hadolint"
+
+fi
+
+# shfmt
+
+if [ ! -x "${HOME}/.local/bin/shfmt" ]; then
+
+  shfmt_base=https://github.com/mvdan/sh/releases/download
+  wget -nv -O "${HOME}/.local/bin/shfmt" \
+    "${shfmt_base}/${SHFMT_VERSION}/shfmt_${SHFMT_VERSION}_linux_amd64"
+  chmod +x "${HOME}/.local/bin/shfmt"
+
+fi
